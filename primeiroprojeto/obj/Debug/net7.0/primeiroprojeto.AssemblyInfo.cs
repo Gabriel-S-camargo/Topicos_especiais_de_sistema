e2caae437e7846aa0b2a20af13e572e7c7457f8d
Santos <gabrielsantoscamargo8@gmail.com>
2024-03-05 21:33:54 -0300

    teste

--- primeiroprojeto/obj/Debug/net7.0/primeiroprojeto.AssemblyInfo.cs
+++ primeiroprojeto/obj/Debug/net7.0/primeiroprojeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("primeiroprojeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84b1d447be85aaabef65a36163cf82aeccbf04c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f24d70134d6c92f12fbb79a7dcc07c27fdcbda0")]
 [assembly: System.Reflection.AssemblyProductAttribute("primeiroprojeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("primeiroprojeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
