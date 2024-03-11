cfab9afe0e17be422b5e694e251eeb8d71392659
Santos <gabrielsantoscamargo8@gmail.com>
2024-03-11 20:04:45 -0300

    Adicao Aula 02

---
+++ aula01/Cliente.cs
@@ -0,0 +1,9 @@
+public class Cliente{
+    public string Nome {get; set;}
+    public string Cpf {get; set;}
+
+    public Cliente(){
+        Nome = "";
+        Cpf = ""; 
+    }
+} 
--- primeiroprojeto/cliente.cs
+++
@@ -1,9 +0,0 @@
-public class Cliente{
-    public string Nome {get; set;}
-    public string Cpf {get; set;}
-
-    public Cliente(){
-        Nome = "";
-        Cpf = ""; 
-    }
-} 
