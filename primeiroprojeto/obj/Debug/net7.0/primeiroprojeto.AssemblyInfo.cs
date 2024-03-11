50e57818610b6e0310a37f23388f3e00e9c5207a
Santos <gabrielsantoscamargo8@gmail.com>
2024-03-11 19:26:11 -0300

    ajuste de staged archives

--- primeiroprojeto/obj/Debug/net7.0/primeiroprojeto.AssemblyInfo.cs
+++ primeiroprojeto/obj/Debug/net7.0/primeiroprojeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("primeiroprojeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f24d70134d6c92f12fbb79a7dcc07c27fdcbda0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09b105b88acc6f98b4928f606576ed4c7e5f2d7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("primeiroprojeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("primeiroprojeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
