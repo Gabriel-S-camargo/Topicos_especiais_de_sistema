577ed3b32c944c61e1c1b1d99b2a40401cea3164
Santos <gabrielsantoscamargo8@gmail.com>
2024-03-11 21:45:55 -0300

    aula02 Concluida

--- aula02/Program.cs
+++ aula02/Program.cs
@@ -1,3 +1,18 @@
-﻿Estudante aluno = new Estudante("R123", "Gabriel");
+﻿//Implrementação de listas de objetos
+class Program{
+    static void Main(){
+        List<Estudante> Estudantes = new List<Estudante>();
+        // construir 3 estudantes e adicionar na lista estudante
+        Estudante aluno;
+        for(int i = 0; i < 3; i++){
+            aluno = new Estudante("R"+i, "Nome"+i);
+            Estudantes.Add(aluno);
 
-Console.WriteLine(aluno.Nome);
+        }
+
+        foreach(var Estudante in Estudantes){
+            Console.WriteLine(Estudante.Rgm + " " + Estudante.Nome);
+        }
+
+    }
+}
--- aula02/obj/Debug/net8.0/aula02.AssemblyInfo.cs
+++ aula02/obj/Debug/net8.0/aula02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e2a2eca94a038c0a00b85deec91b4bf9bed0443")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a0e18964e295aa42ea3877d367a91def69f8c08")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
