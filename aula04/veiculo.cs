9e5401bfa5a4f1ae61a04cdd37016ca3979e930e
Gabriel Santos <gabrielsantoscamargo8@gmail.com>
2024-04-01 21:16:32 -0300

    Adicionado Comentario do exercicos de prática

--- aula04/veiculo.cs
+++ aula04/veiculo.cs
@@ -8,3 +8,30 @@ public class Veiculo{
         this.motor = m;
     }
 } 
+
+/*
+    Atividade Orientação objetos
+
+    Implemente as classes conforme o enunciado
+
+    1. Classe Produto
+    Atributos - >(Codigo, nome, preco)
+
+    2. Classe cliente 
+    Atributos -> (cpf, nome)
+
+    3.Classe Vendedor
+    Atributos -> (Matricula, cpf, nome)
+
+    4. Classe Venda
+    Atributos (Vendedor, Cliente, Lista de produtos vendidos, total_da_venda)
+
+    Metodos:
+
+    a: Construtor com dependencia do vendedor e do Cliente
+    b: Construtor padrão(Constroi o objeto sem estado)
+    c: Método para adicionar os produtos comprados pelo cliente
+    d: Método para calcular o valor da venda
+
+
+*/
